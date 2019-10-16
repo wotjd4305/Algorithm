70e92be820e68b93a1efa586c1da14b6a5d9cb78
wotjd4305 <wotjd4305@naver.com>
2019-10-16 19:07:48 +0900

    create read.me
    3

---
+++ java-project/src/main/java/proDFS1.java
@@ -0,0 +1,60 @@
+public class proDFS1 {
+        static int plusAndMinus[] = {1, -1};
+        static int answer = 0;
+        static int[] Globalnumbers;
+        static int count =0;
+
+    public static void main(String[] args) {
+
+        int[] number= {1,1,1,1,1,1,1};
+        int target = 3;
+
+        System.out.println(solution(number, target));
+
+
+    }
+
+        public static int solution(int[] numbers, int target) {
+
+            int level = 0;
+            int total = 0;
+            Globalnumbers = numbers;
+            DFS(plusAndMinus[0],target, level, total);
+            DFS(plusAndMinus[1],target, level, total);
+
+
+            return answer;
+
+        }
+
+
+        public static void DFS(int plus_minus, int target ,int level, int total)
+        {
+            count++;
+            //연산
+            total = total + plus_minus*Globalnumbers[level];
+
+            //마무리
+            if(level == Globalnumbers.length-1)
+            {
+                System.out.println("카운트 : " + count);
+                System.out.println("비교전 : " + total + " " + target);
+                if(target == total)
+                {
+                    System.out.println(total +"비교후 토탈");
+                    answer++;
+                    return;
+                }
+                System.out.println(total +"비교후 정답 아닌것");
+
+                return;
+            }
+
+            DFS(plusAndMinus[0], target, level+1, total);
+            DFS(plusAndMinus[1], target, level+1, total);
+
+        }
+    }
+
+
+
