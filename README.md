765db3bf746d9c43bc2f13375d4f755d0d413756
wotjd4305 <wotjd4305@naver.com>
2019-11-10 08:36:09 +0900

    swa 2 sol

--- README.md
+++ README.md
@@ -55,6 +55,8 @@ Language: JAVA
 * 1216 - [회문2,문자열](java-project/src/main/java/swe1216.java)
 * 1217 - [제곱,Math함수](java-project/src/main/java/swe1217.java)
 * 1220 - [Magenetic,규칙](java-project/src/main/java/swe1220.java)
+* 1221 - [GNS,HashMap](java-project/src/main/java/swe1221.java)
+* 1234 - [비밀번호,스택](java-project/src/main/java/swe1234.java)
 
 
 ​    
---
+++ java-project/src/main/java/swe1234.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+public class swe1234 {
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        for (int T = 1; T <= 10; T++) {
+            sc.next();
+            String str = sc.next();
+            String answer = "";
+            char[] cArr = new char[str.length()];
+            Stack<Character> Cstack = new Stack<>();
+
+
+            //나눠담기
+            for(int i =0; i<str.length(); i++)
+            {
+                cArr[i] = str.charAt(i);
+            }
+
+
+            Cstack.push(cArr[0]);
+            for(int i=1; i<str.length(); i++)
+            {
+                try {
+                    if(Cstack.empty())
+                        Cstack.push(cArr[i]);
+                    else if(Cstack.peek() == cArr[i])
+                        Cstack.pop();
+                    else
+                        Cstack.push(cArr[i]);
+                }catch (Exception e)
+                {
+                    Cstack.push(cArr[i]);
+                    e.printStackTrace();
+                }
+            }
+
+            while(!Cstack.empty())
+                answer = Cstack.pop() + answer;
+            System.out.println("#" + T + " " + answer);
+
+        }
+    }
+}
+
+
